732740c833d5a79c28b1a0d7bd849cb4fd4e1247
Srinivas Borigarla <iamborigarla@Srinivass-MacBook-Pro.local>
2023-03-24 06:23:08 +0530

    updated voting

--- Contracts/vote.sol
+++ Contracts/vote.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
+pragma solidity ^0.8.7;
 
 contract Voting {
 
@@ -8,13 +7,26 @@ contract Voting {
 
     bytes32[] public projectList;
 
+    constructor(bytes32[] memory projectNames) {
+        projectList = projectNames;
+    }
+ 
     function totalVotesFor(bytes32 project) view public returns (uint256) {
+        require(validProject(project));
         return votesReceived[project];
     }
 
     function voteForProject(bytes32 project) public {
+        require(validProject(project));
         votesReceived[project] += 1;
-        projectList.push(project);
     }
 
+    function validProject(bytes32 project) view public returns (bool) {
+        for(uint i=0; i < projectList.length; i++) {
+            if (projectList[i] == project) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
